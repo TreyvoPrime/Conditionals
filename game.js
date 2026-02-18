cfeaa869116b9ecb9c651b5fdb4fba10159c807d
Treyvon Pearson <tpearson3@my.waketech.edu>
2026-02-18 14:03:03 -0500

    first

---
+++ game.js
@@ -0,0 +1,40 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let userGuess = prompt("guess the number")
+
+console.log(randomNumber)
+let attempts = 0;
+userGuess = Number(userGuess)
+while (userGuess != randomNumber) {
+    let userGuess = prompt("guess the number")
+
+    if (userGuess == randomNumber) {
+        alert(`Congratulations, you've guessed it in ${userGuess}!`);
+
+        break
+    }
+    else if (userGuess > randomNumber && userGuess != 999) {
+        alert("Too high");
+        continue;
+    }
+    else if (userGuess < randomNumber) {
+        alert("Too low");
+        continue;
+    }
+    else if (userGuess = 999) {
+        break
+    }
+    else {
+        alert("Invalid Input")
+    }
+}
+// Prompt user for a number between 1 and 10, or type 999 to exit.
+// Create while loop that continues for as long as the user guesses are incorrect.
+// Loop should count the number of guesses.
+// Loop should exit if user enters 999 and not display "Too high!"
+// If the guess is too low, the user should be promped "Too low!" and be able to guess again.
+// If the guess is too high, the user should be promped 'Too high!" and be able to guess again.
+// If anything else is entered, the user should be prompted that their input is invalid and 
+// to guess again.
+
+// If the guess is correct, the user should be alerted of their win and 
+// be told the number of guesses they made.
